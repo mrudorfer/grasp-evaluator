47c2517520357a7271be5ec52894384512ff384d
Martin Rudorfer <m.rudorfer@bham.ac.uk>
2022-03-03 17:13:43 +0000

    check for simulator, rename to grasp-evaluator

--- setup.py
+++ setup.py
@@ -10,11 +10,10 @@ requirements_default = [
     'tqdm',  # progress bars
     'matplotlib',  # precision coverage plots
     'attrdict',
-    'GPNet-simulator @ git+ssh://git@github.com/mrudorfer/GPNet-simulator'
 ]
 
 setuptools.setup(
-    name='GPNet-evaluator',
+    name='grasp-evaluator',
     version='0.1',
     python_requires=python_versions,
     install_requires=requirements_default,
@@ -23,6 +22,14 @@ setuptools.setup(
     license='',
     author='Martin Rudorfer',
     author_email='m.rudorfer@bham.ac.uk',
-    description='evaluation on GPNet dataset',
+    description='module for rule- and simulation-based evaluation of grasp predictions',
     long_description=long_description
 )
+
+try:
+    import gpnet_sim
+except ImportError:
+    print('*****************************************************************************')
+    print('Please install GPNet-simulator (https://github.com/mrudorfer/GPNet-simulator)')
+    print('*****************************************************************************')
+
